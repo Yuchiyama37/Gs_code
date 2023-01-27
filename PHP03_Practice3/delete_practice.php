a1331ee55458d4a6b39a4a14d04b4882d95c1ce7
Yuchiyama37 <hakkaisan37@gmail.com>
2023-01-27 20:17:20 +0900

    提出

---
+++ PHP03_Practice3/delete_practice.php
@@ -0,0 +1,24 @@
+<?php
+//selsect.phpから処理を持ってくる
+//1.対象のIDを取得
+$id = $_GET['id'];
+
+//2.DB接続します
+require_once('funcs_practice.php');
+$pdo = db_conn();
+
+//3.削除SQLを作成
+$stmt = $pdo->prepare("DELETE FROM inventory_pt WHERE id=:id");
+$stmt->bindvalue(':id',$id, PDO::PARAM_INT);//('仮変数', '代入する変数', 入ってきたvalueを何形式にするか)
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if($status==false){
+    //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+    $error = $stmt->errorInfo();
+    exit("ErrorMassage:".$error[2]);
+  }else{
+    //５．select.phpへリダイレクト
+    redirect('select_practice.php');
+  }
+
---
+++ PHP03_Practice3/ex.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang='ja'>
+<head>
+<meta charset='UTF-8'>
+<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<title>Document</title>
+</head>
+<body>
+
+
+
+
+
+<!-- Main[Start] -->
+<form method="POST" action="insert_practice.php">
+<table>
+<tr class="Chiba">
+<th>出荷地</th>
+<td><input name="terminal_name" type="text" value="千葉"></td>
+<th>出荷日</th>
+<td><input name="ex_date" type="date"></td>
+<th>品種</th>
+<td>
+    <select name="grade" id="">
+        <option value="プロパン">プロパン</option>
+        <option value="ブタン">ブタン</option>
+    </select>
+</td>
+<th>数量</th>
+<td><input name="quantity" type="text"></td>
+<th>出荷形態</th>
+<td>
+    <select name="delivery_mode" id="deliveryMode">
+        <option value="L/Y">L/Y</option>
+        <option value="C/T">C/T</option>
+    </select>
+</td>
+<th>取引形態</th>
+<td>
+    <select name="deal_mode" id="dealMode">
+        <option value="販売">販売</option>
+        <option value="バーター">バーター</option>
+    </select>
+</td>
+<th><input type="submit" value="登録"></th>
+</tr>
+
+
+
+</table>
+
+</form>
+<!-- Main[End] -->
+
+</body>
+</html>
